db0baa11e8b5a7d022de7154cb953f41d59d2124
Aulas-Bilinguismo-SJN <aulas.bilinguismo.sjn@gmail.com>
2025-07-22 03:44:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxPkJVdzy3dmbyfT8jUb
 
 // Variables para control de actualizaciones
 let ultimaActualizacion = 0;
-const INTERVALO_ACTUALIZACION = 10000; // 30 segundos
+const INTERVALO_ACTUALIZACION = 30000; // 30 segundos
 
 // --- UTILIDADES ---
 function mostrarNotificacion(mensaje, tipo = 'info') {
